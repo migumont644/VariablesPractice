5274efdffa0e42214eb187a124ff8cc04321fa91
Trooper M <migmon23@hotmail.com>
2021-02-09 14:13:01 -0500

    Add project files.

---
+++ VariablesPractice/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace VariablesPractice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonBackground = new System.Windows.Forms.Label();
+            this.hockeyButton = new System.Windows.Forms.Button();
+            this.areaButton = new System.Windows.Forms.Button();
+            this.carpetButton = new System.Windows.Forms.Button();
+            this.billButton = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonBackground
+            // 
+            this.buttonBackground.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.buttonBackground.Location = new System.Drawing.Point(2, -1);
+            this.buttonBackground.Name = "buttonBackground";
+            this.buttonBackground.Size = new System.Drawing.Size(182, 453);
+            this.buttonBackground.TabIndex = 0;
+            // 
+            // hockeyButton
+            // 
+            this.hockeyButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hockeyButton.Location = new System.Drawing.Point(31, 39);
+            this.hockeyButton.Name = "hockeyButton";
+            this.hockeyButton.Size = new System.Drawing.Size(133, 49);
+            this.hockeyButton.TabIndex = 1;
+            this.hockeyButton.Text = "Hockey";
+            this.hockeyButton.UseVisualStyleBackColor = true;
+            this.hockeyButton.Click += new System.EventHandler(this.HockeyButton_Click);
+            // 
+            // areaButton
+            // 
+            this.areaButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.areaButton.Location = new System.Drawing.Point(31, 138);
+            this.areaButton.Name = "areaButton";
+            this.areaButton.Size = new System.Drawing.Size(133, 49);
+            this.areaButton.TabIndex = 2;
+            this.areaButton.Text = "Area";
+            this.areaButton.UseVisualStyleBackColor = true;
+            this.areaButton.Click += new System.EventHandler(this.AreaButton_Click);
+            // 
+            // carpetButton
+            // 
+            this.carpetButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.carpetButton.Location = new System.Drawing.Point(31, 241);
+            this.carpetButton.Name = "carpetButton";
+            this.carpetButton.Size = new System.Drawing.Size(133, 49);
+            this.carpetButton.TabIndex = 3;
+            this.carpetButton.Text = "Carpet";
+            this.carpetButton.UseVisualStyleBackColor = true;
+            this.carpetButton.Click += new System.EventHandler(this.CarpetButton_Click);
+            // 
+            // billButton
+            // 
+            this.billButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.billButton.Location = new System.Drawing.Point(31, 342);
+            this.billButton.Name = "billButton";
+            this.billButton.Size = new System.Drawing.Size(133, 49);
+            this.billButton.TabIndex = 4;
+            this.billButton.Text = "Bill";
+            this.billButton.UseVisualStyleBackColor = true;
+            this.billButton.Click += new System.EventHandler(this.BillButton_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.BackColor = System.Drawing.Color.White;
+            this.outputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.Location = new System.Drawing.Point(224, 39);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(540, 352);
+            this.outputLabel.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.billButton);
+            this.Controls.Add(this.carpetButton);
+            this.Controls.Add(this.areaButton);
+            this.Controls.Add(this.hockeyButton);
+            this.Controls.Add(this.buttonBackground);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label buttonBackground;
+        private System.Windows.Forms.Button hockeyButton;
+        private System.Windows.Forms.Button areaButton;
+        private System.Windows.Forms.Button carpetButton;
+        private System.Windows.Forms.Button billButton;
+        private System.Windows.Forms.Label outputLabel;
+    }
+}
+
---
+++ VariablesPractice/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VariablesPractice
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void HockeyButton_Click(object sender, EventArgs e)
+        {
+            int wayneGretzky = 99;
+            string hockeyName = "Wayne Gretzky is number";
+            outputLabel.Text = $"{hockeyName} {wayneGretzky}";
+
+        }
+
+        private void AreaButton_Click(object sender, EventArgs e)
+        {      
+            double radius = 15;
+            double pi = 3.14;
+            double totalRadius = radius * radius;
+            double area = totalRadius * pi;
+
+            outputLabel.Text = $"The area of circle with a radius of {radius} is {area}^2";
+
+        }
+
+        private void CarpetButton_Click(object sender, EventArgs e)
+        {
+            double length = 8.5;
+            double width = 6;
+            double costPerMeter = 19.95;
+            double area = length * width;
+            double totalCost = area * costPerMeter;
+
+            outputLabel.Text = $"The area of a room with dimensions {length}m x {width}m is {area}m^2";
+            outputLabel.Text += $"\n\nThe cost to carpet this area at {costPerMeter} per square metre is {totalCost}";
+        }
+
+        private void BillButton_Click(object sender, EventArgs e)
+        {
+            double shirtPrice = 12.49;
+            double tendered = 20.00;
+            double tax = 1.62;
+            double total = tax + shirtPrice;
+            double charge = tendered - total;
+
+            outputLabel.Text = "Bill of Sale";
+            outputLabel.Text += $"\n\nShirt:      {shirtPrice}";
+            outputLabel.Text += $"\n\nTax:         {tax}";
+            outputLabel.Text += $"\nTotal:      {total}";
+            outputLabel.Text += $"\n\nTendered:      {tendered}";
+            outputLabel.Text += $"\nChange:      {charge}";
+        }  
+    }
+}
